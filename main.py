206e0daaca8a14a9497db4082988a33ec331842c
Imran0511 <mohdimranshountoo@gmail.com>
2025-08-12 14:56:06 +0530

    Update application port configuration to 8080 in apprunner.yaml, main.py, and run_local.py for consistency across environments.

--- main.py
+++ main.py
@@ -60,7 +60,7 @@ async def echo_message(message: dict):
     }
 
 if __name__ == "__main__":
-    port = int(os.getenv("PORT", 8000))
+    port = int(os.getenv("PORT", 8080))
     uvicorn.run(
         "main:app",
         host="0.0.0.0",
--- run_local.py
+++ run_local.py
@@ -23,8 +23,8 @@ if __name__ == "__main__":
     
     uvicorn.run(
         "main:app",
-        host="127.0.0.1",
-        port=8000,
+        host="0.0.0.0",
+        port=8080,
         reload=True,
         reload_dirs=["./"],
         log_level="info"
